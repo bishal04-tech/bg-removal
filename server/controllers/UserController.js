29199b1ee1ea8d71f1f631d9e2d948f7879a1991
bishal04-tech <bishalmahanta68@gmail.com>
2025-11-20 19:40:22 +0530

    third commit

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -2,7 +2,7 @@ import { Webhook } from "svix"
 import userModel from "../models/userModel.js"
 import transactionModel from "../models/transactionModel.js"
 import razorpay from 'razorpay';
-import stripe from "stripe";
+
 
 // Gateway Initialize
 
